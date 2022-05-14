7e6bac567d4c50c5110ef738246d55dc9171c9f6
Ralph Markham <rem@rogers.com>
2022-05-14 15:14:31 -0400

    fixed unit test

--- dji-config/src/test/java/ca/ralphsplace/djiconfig/DjiConfigApplicationTest.java
+++ dji-config/src/test/java/ca/ralphsplace/djiconfig/DjiConfigApplicationTest.java
@@ -2,26 +2,17 @@ package ca.ralphsplace.djiconfig;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
-
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class DjiConfigApplicationTest {
-    @Test
-    void contextLoads() throws Exception {
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create("http://localhost:8191/dji-stocks-data/dev"))
-            .build();
-        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-            .thenApply(HttpResponse::body)
-            .thenAccept(a -> Assertions.assertTrue(a.indexOf("\"spring.data.mongodb.host\":\"mongoDB\"") > -1))
-            .join();
+    @Test
+    void contextLoads(@Autowired TestRestTemplate restTemplate) {
+        String body = restTemplate.getForObject("/dji-stocks-data/dev", String.class);
+        Assertions.assertTrue(body.contains("\"spring.data.mongodb.host\":\"mongoDB\""));
     }
 }
