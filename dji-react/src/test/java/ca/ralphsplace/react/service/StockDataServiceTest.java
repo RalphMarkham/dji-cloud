f684d8fbb4aee48a6eeaba7261818cd68b16b299
Ralph Markham <rem@rogers.com>
2022-06-05 18:14:55 -0400

    updated ot test for blocking

--- dji-react/src/test/java/ca/ralphsplace/react/service/StockDataServiceTest.java
+++ dji-react/src/test/java/ca/ralphsplace/react/service/StockDataServiceTest.java
@@ -12,10 +12,12 @@ import de.flapdoodle.embed.mongo.config.Net;
 import de.flapdoodle.embed.mongo.distribution.Version;
 import de.flapdoodle.embed.process.runtime.Network;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.reactivestreams.Publisher;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import reactor.blockhound.BlockHound;
 import reactor.test.StepVerifier;
 
 class StockDataServiceTest extends UnitTestConstants {
@@ -24,6 +26,11 @@ class StockDataServiceTest extends UnitTestConstants {
 
     private MongodExecutable mongodExecutable;
     private StockDataService stockDataService;
+
+    @BeforeAll
+    static void beforeClass() {
+        BlockHound.install();
+    }
 
     @AfterEach
     void clean() {
